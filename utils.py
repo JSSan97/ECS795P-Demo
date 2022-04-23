e7129e686c14fe2a2e74d53511d64ab759d08862
JSSan97 <j.san@hotmail.co.uk>
2022-04-23 15:06:37 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ def load_image_from_tensor(image, save_path=None, title=None):
     plt.imshow(np.transpose(np_image, (1, 2, 0)))
     ## Display image
     plt.show()
+    plt.pause(0.1)
 
     if save_path:
         print("Images saved in: {}".format(save_path))
