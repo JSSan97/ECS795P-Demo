962e83ebd8bd17db5ec1c845b8308cc5baa57cdb
JSSan97 <j.san@hotmail.co.uk>
2022-04-24 15:26:16 +0100

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -122,12 +122,10 @@ def get_input_transform(model_name):
             transforms.RandomHorizontalFlip(),
             transforms.RandomCrop(32, padding=4),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            transforms.Normalize(mean=(0.5,), std=(0.5,)),
         ])
 
-        transform_test = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        ])
+        transform_test = transforms.Compose([transforms.ToTensor(),
+            transforms.Normalize(mean=(0.5,), std=(0.5,))])
 
     return transform_train, transform_test
