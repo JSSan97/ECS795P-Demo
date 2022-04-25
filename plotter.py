b1abb56a94a4b1feebe0ad02264db7d4491bc84e
JSSan97 <j.san@hotmail.co.uk>
2022-04-25 17:04:18 +0100

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,27 +1,28 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 HISTORY_PATH = {
-    "VGG16" : {
-        "MNIST" : "/MNIST_results/VGG16/training_history_30.npy",
-        "MNISTFashion" : "/MNISTFashion_results/VGG16/training_history_30.npy",
-        "CIFAR10" : "/CIFAR10_results/VGG16/training_history_30.npy",
+    'VGG16' : {
+        'MNIST' : 'MNIST_results\VGG16\\training_history_30.npy',
+        'MNISTFashion' : 'MNISTFashion_results\VGG16\\training_history_30.npy',
+        'CIFAR10' : 'CIFAR10_results\VGG16\\training_history_30.npy',
     },
-    "VGG13": {
-        "MNIST": "/MNIST_results/VGG13/training_history_30.npy",
-        "MNISTFashion": "/MNISTFashion_results/VGG13/training_history_30.npy",
-        "CIFAR10": "/CIFAR10_results/VGG13/training_history_30.npy",
+    'VGG13': {
+        'MNIST': 'MNIST_results\VGG13\\training_history_30.npy',
+        'MNISTFashion': 'MNISTFashion_results\VGG13\\training_history_30.npy',
+        'CIFAR10': 'CIFAR10_results\VGG13\\training_history_30.npy',
     },
-    "ResNet101": {
-        "MNIST": "/MNIST_results/ResNet101/training_history_30.npy",
-        "MNISTFashion": "/MNISTFashion_results/ResNet101/training_history_30.npy",
-        "CIFAR10": "/CIFAR10_results/ResNet101/training_history_30.npy",
+    'ResNet101': {
+        'MNIST': 'MNIST_results\ResNet101\\training_history_30.npy',
+        'MNISTFashion': 'MNISTFashion_results\\ResNet101\training_history_30.npy',
+        'CIFAR10': 'CIFAR10_results\\ResNet101\training_history_30.npy',
     },
-    "ResNet50": {
-        "MNIST": "/MNIST_results/ResNet50/training_history_30.npy",
-        "MNISTFashion": "/MNISTFashion_results/ResNet50/training_history_30.npy",
-        "CIFAR10": "/CIFAR10_results/ResNet50/training_history_30.npy",
+    'ResNet50': {
+        'MNIST': 'MNIST_results\ResNet50\\training_history_30.npy',
+        'MNISTFashion': 'MNISTFashion_results\ResNet50\\training_history_30.npy',
+        'CIFAR10': 'CIFAR10_results\ResNet50\\training_history_30.npy',
     }
 }
 
@@ -31,14 +32,17 @@ def plotter(save=True, show=True):
     parser.add_argument('--model_name', type=str, default='VGG13', choices=['VGG13', 'VGG16', 'ResNet50', 'ResNet101', 'GoogLeNet'], help='Name of architecture')
     opt = parser.parse_args()
 
-    for dataset, path in HISTORY_PATH.get(opt.model_name):
-        train_history = np.load('{}'.format(path), allow_pickle=True)
+    current_path = os.path.dirname(os.path.realpath(__file__))
+
+    for dataset, rel_path in HISTORY_PATH.get(opt.model_name).items():
+        print('{}\{}'.format(current_path, rel_path))
+        train_history = np.load('{}\{}'.format(current_path, rel_path), allow_pickle=True)
         train_history = np.ndarray.tolist(train_history)
         x = train_history['time']
         y1 = train_history['train_avg_loss']
         y2 = train_history['test_avg_loss']
-        plt.plot(x, y1, label="Training Loss {}".format(dataset))
-        plt.plot(x, y2, label="Testing Loss {}".format(dataset))
+        plt.plot(x, y1, label='Training Loss {}'.format(dataset))
+        plt.plot(x, y2, label='Testing Loss {}'.format(dataset))
 
 
     plt.xlabel('Time')
@@ -49,7 +53,7 @@ def plotter(save=True, show=True):
     plt.tight_layout()
 
     if save:
-        plt.savefig(path)
+        plt.savefig("Loss_{}".format(opt.model_name))
     if show:
         plt.show()
     else:
