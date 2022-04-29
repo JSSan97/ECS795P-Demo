d53783ba900ea502cf55c25905b391bcf384397c
JSSan97 <j.san@hotmail.co.uk>
2022-04-29 12:16:05 +0100

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -3,47 +3,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-HISTORY_PATH = {
-    'VGG16' : {
-        'MNIST' : 'MNIST_results\VGG16\\training_history_30.npy',
-        'MNISTFashion' : 'MNISTFashion_results\VGG16\\training_history_30.npy',
-        'CIFAR10' : 'CIFAR10_results\VGG16\\training_history_30.npy',
+MNIST_PATH = '/content/drive/MyDrive/Github/ECS795P-Demo/MNIST_results'
+MNISTFASHION_PATH = '/content/drive/MyDrive/Github/ECS795P-Demo/MNISTFashion_results'
+CIFAR10_PATH = '/content/drive/MyDrive/Github/ECS795P-Demo/CIFAR10_results'
+
+BY_DATASET = {
+    'MNIST': {
+        'VGG16': '{}/VGG16/model_VGG16_epoch40.pth'.format(MNIST_PATH),
+        'ResNet101': '{}/ResNet101/model_ResNet101_epoch40.pth'.format(MNIST_PATH),
+        'ResNet101SE': '{}/ResNet101SE/model_ResNet101SE_epoch40.pth'.format(MNIST_PATH),
+        'ResidualAttention56': '{}/ResidualAttention56/model_ResidualAttention56_epoch40.pth'.format(MNIST_PATH),
+        'ResNet101CBAM': '{}/ResNet101CBAM/model_ResNet101CBAM_epoch40.pth'.format(MNIST_PATH),
     },
-    'VGG13': {
-        'MNIST': 'MNIST_results\VGG13\\training_history_30.npy',
-        'MNISTFashion': 'MNISTFashion_results\VGG13\\training_history_30.npy',
-        'CIFAR10': 'CIFAR10_results\VGG13\\training_history_30.npy',
+    'MNISTFASHION': {
+        'VGG16': '{}/VGG16/model_VGG16_epoch40.pth'.format(MNISTFASHION_PATH),
+        'ResNet101': '{}/ResNet101/model_ResNet101_epoch40.pth'.format(MNISTFASHION_PATH),
+        'ResNet101SE': '{}/ResNet101SE/model_ResNet101SE_epoch40.pth'.format(MNISTFASHION_PATH),
+        'ResidualAttention56': '{}/ResidualAttention56/model_ResidualAttention56_epoch40.pth'.format(MNISTFASHION_PATH),
+        'ResNet101CBAM': '{}/ResNet101CBAM/model_ResNet101CBAM_epoch40.pth'.format(MNISTFASHION_PATH),
     },
-    'ResNet101': {
-        'MNIST': 'MNIST_results\ResNet101\\training_history_30.npy',
-        'MNISTFashion': 'MNISTFashion_results\\ResNet101\training_history_30.npy',
-        'CIFAR10': 'CIFAR10_results\\ResNet101\training_history_30.npy',
+    'CIFAR10': {
+        'VGG16': '{}/VGG16/model_VGG16_epoch40.pth'.format(CIFAR10_PATH),
+        'ResNet101': '{}/ResNet101/model_ResNet101_epoch40.pth'.format(CIFAR10_PATH),
+        'ResNet101SE': '{}/ResNet101SE/model_ResNet101SE_epoch40.pth'.format(CIFAR10_PATH),
+        'ResidualAttention56': '{}/ResidualAttention56/model_ResidualAttention56_epoch40.pth'.format(CIFAR10_PATH),
+        'ResNet101CBAM': '{}/ResNet101CBAM/model_ResNet101CBAM_epoch40.pth'.format(CIFAR10_PATH),
     },
-    'ResNet50': {
-        'MNIST': 'MNIST_results\ResNet50\\training_history_30.npy',
-        'MNISTFashion': 'MNISTFashion_results\ResNet50\\training_history_30.npy',
-        'CIFAR10': 'CIFAR10_results\ResNet50\\training_history_30.npy',
-    }
 }
 
 def plotter(save=True, show=True):
     # Arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str, default='VGG13', choices=['VGG13', 'VGG16', 'ResNet50', 'ResNet101', 'GoogLeNet'], help='Name of architecture')
+    parser.add_argument('--dataset', type=str, default='MNIST', choices=['MNIST', 'CIFAR10', 'MNISTFashion'], help='Dataset')
     opt = parser.parse_args()
 
-    current_path = os.path.dirname(os.path.realpath(__file__))
-
-    for dataset, rel_path in HISTORY_PATH.get(opt.model_name).items():
-        print('{}\{}'.format(current_path, rel_path))
-        train_history = np.load('{}\{}'.format(current_path, rel_path), allow_pickle=True)
+    for model, path in BY_DATASET.get(opt.dataset).items():
+        train_history = np.load(path, allow_pickle=True)
         train_history = np.ndarray.tolist(train_history)
         x = train_history['time']
         y1 = train_history['train_avg_loss']
         y2 = train_history['test_avg_loss']
-        plt.plot(x, y1, label='Training Loss {}'.format(dataset))
-        plt.plot(x, y2, label='Testing Loss {}'.format(dataset))
-
+        plt.plot(x, y1, label='Training Loss {}'.format(model))
+        plt.plot(x, y2, label='Testing Loss {}'.format(model))
 
     plt.xlabel('Time')
     plt.ylabel('Loss')
@@ -58,8 +59,6 @@ def plotter(save=True, show=True):
         plt.show()
     else:
         plt.close()
-
-
 
 if __name__ == '__main__':
     plotter()
