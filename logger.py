a46ceede52c97a95469c73c44814a7aee3ce87c0
JSSan97 <j.san@hotmail.co.uk>
2022-05-01 17:35:59 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -9,7 +9,7 @@ def setup_custom_logger(name, file):
     screen_handler = logging.StreamHandler(stream=sys.stdout)
     screen_handler.setFormatter(formatter)
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.DEBUG, force=True)
     logger.addHandler(handler)
     logger.addHandler(screen_handler)
     return logger
