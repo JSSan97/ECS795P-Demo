f9c068ba460b1e3792371749dec6c21b8b8a39b9
JSSan97 <j.san@hotmail.co.uk>
2022-05-03 15:19:08 +0100

    Update experiment_plotter.py

--- experiment_plotter.py
+++ experiment_plotter.py
@@ -50,7 +50,6 @@ def train_loss(dataset, save=True, show=True):
         x = range(len(train_history['train_avg_loss']))
         y1 = train_history['train_avg_loss']
         plt.plot(x, y1, label='Training Loss {}'.format(model))
-        print("Final Train for Model {} is {}".format(model, train_history['train_accuracy'][-1]))
 
     plt.xlabel('Epoch')
     plt.ylabel('Avg Loss')
@@ -75,7 +74,6 @@ def test_loss(dataset, save=True, show=True):
         x = range(len(train_history['train_avg_loss']))
         y1 = train_history['test_avg_loss']
         plt.plot(x, y1, label='Testing Loss {}'.format(model))
-        print("Final Test Validation Accuracy for Model {} is {}".format(model, train_history['test_accuracy'][-1]))
 
     plt.xlabel('Epoch')
     plt.ylabel('Avg Loss')
@@ -86,6 +84,56 @@ def test_loss(dataset, save=True, show=True):
 
     if save:
         plt.savefig("Loss_Test_{}.png".format(dataset))
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+
+def train_accuracy(dataset, save=True, show=True):
+    for model, path in BY_DATASET.get(dataset).items():
+        print("Model {} ".format(model))
+        train_history = np.load(path, allow_pickle=True)
+        train_history = np.ndarray.tolist(train_history)
+        x = range(len(train_history['train_accuracy']))
+        y1 = train_history['train_accuracy']
+        plt.plot(x, y1, label='Training Accuracy {}'.format(model))
+        print("Final Train for Model {} is {}".format(model, train_history['train_accuracy'][-1]))
+
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title('Training Accuracy Over Epoch - {}'.format(dataset))
+    plt.legend(bbox_to_anchor=(1, 1), loc=1, borderaxespad=0)
+    plt.grid(True)
+    plt.tight_layout()
+
+    if save:
+        plt.savefig("Accuracy_Train_{}.png".format(dataset))
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+
+def test_accurcy(dataset, save=True, show=True):
+    for model, path in BY_DATASET.get(dataset).items():
+        print("Model {} ".format(model))
+        train_history = np.load(path, allow_pickle=True)
+        train_history = np.ndarray.tolist(train_history)
+        x = range(len(train_history['test_accuracy']))
+        y1 = train_history['test_accuracy']
+        plt.plot(x, y1, label='Testing Accuracy {}'.format(model))
+        print("Final Test Validation Accuracy for Model {} is {}".format(model, train_history['test_accuracy'][-1]))
+
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title('Test test_accuracy Over Epoch - {}'.format(dataset))
+    plt.legend(bbox_to_anchor=(1, 1), loc=1, borderaxespad=0)
+    plt.grid(True)
+    plt.tight_layout()
+
+    if save:
+        plt.savefig("Accuracy_Test_{}.png".format(dataset))
     if show:
         plt.show()
     else:
